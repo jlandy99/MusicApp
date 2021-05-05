71d3238e5d6b36d5cbb89e6156d384f087521619
jlandy99 <jlandy99@github.com>
2021-05-05 13:08:48 -0400

    First commit, added frameworks from Spotify SDK

---
+++ MusicApp/MusicApp-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MusicApp-Bridging-Header.h
+//  MusicApp
+//
+//  Created by John Landy on 5/5/21.
+//
+
+#ifndef MusicApp_Bridging_Header_h
+#define MusicApp_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* MusicApp_Bridging_Header_h */
